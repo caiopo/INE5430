ce312abff815547aec4bffbb488d93edbee04483
Salomão Rodrigues Jacinto <MaoRodriguesJ@users.noreply.github.com>
2017-05-07 19:13:33 -0300

    Add animals

--- T2/animals.py
+++ T2/animals.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 from os import path
-from properties import Colors, SkinTypes, Origins, Size
 from collections import Counter
 
 
@@ -28,18 +27,18 @@ WHITE = 'Branco'
 YELLOW = 'Amarelo'
 
 # Skin types
-DRY = 'Seca'
-FEATHER = 'Pena'
+DRY = 'Couro'
+FEATHER = 'Penas'
 FUR = 'Pêlo'
-SCALE = 'Escama'
+SCALE = 'Escamas'
 SHELL = 'Casco'
 WET = 'Úmida'
 
 # Habitat
 DESERT = 'Deserto'
 FRESHWATER = 'Água Doce'
-GRASSLAND = 'Grassland'
-MOUNTAINS = 'Montanha'
+GRASSLAND = 'Planície'
+MOUNTAIN = 'Montanha'
 OCEAN = 'Oceano'
 POLAR = 'Pólo Norte/Sul'
 SWAMP = 'Pântano'
@@ -59,99 +58,102 @@ LARGE = 'Grande'
 # Diet
 CARNIVORE = 'Carnívoro'
 HERBIVORE = 'Herbívoro'
-OMNIVORE = 'Omnívoro'
+OMNIVORE = 'Onívoro'
 
 IGNORE = 'ignore'
 YES = 'sim'
 NO = 'não'
 
-# alpaca
-# anta
-# antilope
+
 # burro
-# camelo
-# canguru
 # capivara
-# cavalo marinho
 # cervo
 # chipanze
-# coelho
-# corvo
-# flamingo
-# foca
-# gaivota
-# galinha
+# esquilo
 # guaxinim
+# gorila
 # hiena
-# hipopotamo
-# humanos
+# iguana
 # jaguatirica
-# leao marinho
 # lemure
 # lontra
-# morcego
-# mula
 # onca
-# ornitorrinco
-# ovelha
-# porco
-# porco espinho
 # quati
-# rato
-# rinoceronte
 # tamanduá
-# tatu
-# tigre
-# unicornio
-# urso
-# zebra
+
 
 _animals = (
-    ('Peixinho-dourado', FISH, ORANGE, SCALE, FRESHWATER, TINY,   OMNIVORE),
-    ('Orca',             FISH, BLACK,  WET,   OCEAN,  LARGE,  CARNIVORE),
-    ('Salmão',           FISH, GREY,   SCALE, OCEAN,  SMALL,  OMNIVORE),
-    ('Tubarão',          FISH, GREY,   WET,   OCEAN,  LARGE,  CARNIVORE),
-    ('Boto',             FISH, PINK,   WET,   OCEAN,  MEDIUM, CARNIVORE),
-    ('Tainha',           FISH, GREY,   WET,   OCEAN,  SMALL,  OMNIVORE),
-    ('Golfinho',         FISH, GREY,   WET,   OCEAN,  LARGE,  CARNIVORE),
+    ('Peixinho-dourado', FISH, ORANGE, SCALE, FRESHWATER, TINY,   OMNIVORE,  NO),
+    ('Orca',             FISH, BLACK,  WET,   OCEAN,      LARGE,  CARNIVORE, YES),
+    ('Salmão',           FISH, GREY,   SCALE, OCEAN,      SMALL,  OMNIVORE,  NO),
+    ('Tubarão',          FISH, GREY,   WET,   OCEAN,      LARGE,  CARNIVORE, YES),
+    ('Boto',             FISH, PINK,   WET,   OCEAN,      MEDIUM, CARNIVORE, IGNORE),
+    ('Tainha',           FISH, GREY,   WET,   OCEAN,      SMALL,  OMNIVORE,  NO),
+    ('Golfinho',         FISH, GREY,   WET,   OCEAN,      LARGE,  CARNIVORE, IGNORE),
+    ('Cavalo Marinho',   FISH, IGNORE, WET,   OCEAN,      TINY,   CARNIVORE, NO),
+    ('Piranha',          FISH, GREY,   WET,   OCEAN,      SMALL,  CARNIVORE, YES),
 
-    # revisar anfíbios
-    ('Sapo',       AMPHIBIAN, GREEN, WET, IGNORE, SMALL, CARNIVORE),
-    ('Pereca',     AMPHIBIAN, GREEN, WET, IGNORE, SMALL, CARNIVORE),
-    ('Rã',         AMPHIBIAN, GREEN, WET, IGNORE, SMALL, CARNIVORE),
-    ('Rã',         AMPHIBIAN, BROWN, WET, IGNORE, SMALL, CARNIVORE),
-    ('Salamandra', AMPHIBIAN, GREEN, WET, IGNORE, SMALL, CARNIVORE),
+    ('Sapo',       AMPHIBIAN, GREEN, WET, FOREST, SMALL, CARNIVORE, NO),
+    ('Pereca',     AMPHIBIAN, GREEN, WET, FOREST, SMALL, CARNIVORE, NO),
+    ('Rã',         AMPHIBIAN, GREEN, WET, FOREST, SMALL, CARNIVORE, NO),
+    ('Rã',         AMPHIBIAN, BROWN, WET, FOREST, SMALL, CARNIVORE, NO),
+    ('Salamandra', AMPHIBIAN, GREEN, WET, FOREST, SMALL, CARNIVORE, IGNORE),
 
-    ('Crocodilo', REPTILE, GREEN,  DRY,   IGNORE, LARGE, CARNIVORE),
-    ('Jacaré',    REPTILE, GREEN,  DRY,   IGNORE, LARGE, CARNIVORE),
-    ('Tartaruga', REPTILE, BROWN,  SHELL, IGNORE, SMALL, OMNIVORE),
-    ('Lagarto',   REPTILE, IGNORE, SCALE, IGNORE, SMALL, OMNIVORE),
-    ('Jibóia',    REPTILE, BROWN,  DRY,   IGNORE, LARGE, CARNIVORE),
-    ('Dragão',    REPTILE, IGNORE, SCALE, IGNORE, LARGE, CARNIVORE),
+    ('Crocodilo', REPTILE, GREEN,  DRY,   SWAMP,  LARGE, CARNIVORE, YES),
+    ('Jacaré',    REPTILE, GREEN,  DRY,   SWAMP,  LARGE, CARNIVORE, YES),
+    ('Tartaruga', REPTILE, BROWN,  SHELL, OCEAN,  SMALL, OMNIVORE,  NO),
+    ('Lagarto',   REPTILE, IGNORE, SCALE, FOREST, SMALL, OMNIVORE,  NO),
+    ('Cobra',     REPTILE, BROWN,  DRY,   FOREST, LARGE, CARNIVORE, YES),
 
-    ('Pato',     BIRD, IGNORE, FEATHER, IGNORE,  SMALL,  OMNIVORE),
-    ('Águia',    BIRD, BROWN,  FEATHER, AMERICA, MEDIUM, CARNIVORE),
-    ('Falcão',   BIRD, BROWN,  FEATHER, IGNORE,  SMALL,  CARNIVORE),
-    ('Cisne',    BIRD, WHITE,  FEATHER, AMERICA, IGNORE, OMNIVORE),
-    ('Papagaio', BIRD, GREEN,  FEATHER, IGNORE, IGNORE, HERBIVORE),
-    ('Pinguim',  BIRD, BLACK,  FEATHER, IGNORE, IGNORE, OMNIVORE),
-    ('Coruja',   BIRD, WHITE,  FEATHER, IGNORE, IGNORE, OMNIVORE),
-    ('Gaviao',   BIRD, WHITE,  FEATHER, IGNORE, IGNORE, OMNIVORE),
-    ('Picapau',  BIRD, WHITE,  FEATHER, IGNORE, IGNORE, OMNIVORE),
-    ('Avestruz', BIRD, WHITE,  FEATHER, IGNORE, IGNORE, OMNIVORE),
-    ('Ema',      BIRD, WHITE,  FEATHER, IGNORE, IGNORE, OMNIVORE),
+    ('Pato',     BIRD, IGNORE, FEATHER, IGNORE,  SMALL,  OMNIVORE,  NO),
+    ('Águia',    BIRD, BROWN,  FEATHER, IGNORE,  MEDIUM, CARNIVORE, YES),
+    ('Falcão',   BIRD, BROWN,  FEATHER, IGNORE,  SMALL,  CARNIVORE, YES),
+    ('Cisne',    BIRD, WHITE,  FEATHER, IGNORE,  SMALL,  OMNIVORE,  NO),
+    ('Papagaio', BIRD, GREEN,  FEATHER, FOREST,  SMALL,  HERBIVORE, NO),
+    ('Pinguim',  BIRD, BLACK,  FEATHER, POLAR,   MEDIUM, OMNIVORE,  NO),
+    ('Coruja',   BIRD, WHITE,  FEATHER, FOREST,  SMALL,  OMNIVORE,  NO),
+    ('Gaviao',   BIRD, WHITE,  FEATHER, IGNORE,  MEDIUM, OMNIVORE,  YES),
+    ('Picapau',  BIRD, WHITE,  FEATHER, IGNORE,  SMALL,  OMNIVORE,  NO),
+    ('Avestruz', BIRD, WHITE,  FEATHER, IGNORE,  LARGE,  OMNIVORE,  NO),
+    ('Ema',      BIRD, WHITE,  FEATHER, IGNORE,  LARGE,  OMNIVORE,  NO),
+    ('Corvo',    BIRD, BLACK,  FEATHER, IGNORE,  SMALL,  OMNIVORE,  NO),
+    ('Flamingo', BIRD, PINK,   FEATHER, IGNORE,  MEDIUM, CARNIVORE, NO),
+    ('Gaivota',  BIRD, IGNORE, FEATHER, IGNORE,  SMALL,  CARNIVORE, NO),
+    ('Galinha',  BIRD, BROWN,  FEATHER, URBAN,   SMALL,  OMNIVORE,  NO),
+    ('Tucano',   BIRD, BLACK,  FEATHER, FOREST,  MEDIUM, OMNIVORE,  NO),
 
-    ('Macaco',      MAMMAL, BROWN,  FUR, OCEAN,  LARGE,  OMNIVORE),
-    ('Baleia',      MAMMAL, GREY,   WET, OCEAN,  LARGE,  CARNIVORE),
-    ('Gato',        MAMMAL, IGNORE, FUR, IGNORE, SMALL,  CARNIVORE),
-    ('Vaca',        MAMMAL, IGNORE, FUR, IGNORE, MEDIUM, CARNIVORE),
-    ('Cachorro',    MAMMAL, IGNORE, FUR, IGNORE, SMALL,  CARNIVORE),
-    ('Elefante',    MAMMAL, GREY,   DRY, AFRICA, LARGE,  HERBIVORE),
-    ('Girafa',      MAMMAL, BROWN,  FUR, AFRICA, LARGE,  HERBIVORE),
-    ('Cavalo',      MAMMAL, IGNORE, FUR, IGNORE, MEDIUM, HERBIVORE),
-    ('Leão',        MAMMAL, YELLOW, FUR, AFRICA, MEDIUM, CARNIVORE),
+    ('Macaco',       MAMMAL, BROWN,  FUR,    FOREST,     LARGE,  OMNIVORE,  NO),
+    ('Baleia',       MAMMAL, GREY,   WET,    OCEAN,      LARGE,  CARNIVORE, YES),
+    ('Gato',         MAMMAL, IGNORE, FUR,    URBAN,      SMALL,  CARNIVORE, NO),
+    ('Vaca',         MAMMAL, IGNORE, FUR,    GRASSLAND,  MEDIUM, CARNIVORE, NO),
+    ('Cachorro',     MAMMAL, IGNORE, FUR,    URBAN,      SMALL,  CARNIVORE, NO),
+    ('Elefante',     MAMMAL, GREY,   DRY,    GRASSLAND,  LARGE,  HERBIVORE, IGNORE),
+    ('Girafa',       MAMMAL, BROWN,  FUR,    GRASSLAND,  LARGE,  HERBIVORE, NO),
+    ('Cavalo',       MAMMAL, IGNORE, FUR,    GRASSLAND,  MEDIUM, HERBIVORE, NO),
+    ('Leão',         MAMMAL, YELLOW, FUR,    GRASSLAND,  MEDIUM, CARNIVORE, YES),
+    ('Alpaca',       MAMMAL, IGNORE, FUR,    MOUNTAIN,   MEDIUM, HERBIVORE, NO),
+    ('Camelo',       MAMMAL, YELLOW, FUR,    DESERT,     MEDIUM, HERBIVORE, NO),
+    ('Zebra',        MAMMAL, BLACK,  FUR,    GRASSLAND,  MEDIUM, HERBIVORE, NO),
+    ('Zebra',        MAMMAL, WHITE,  FUR,    GRASSLAND,  MEDIUM, HERBIVORE, NO),
+    ('Canguru',      MAMMAL, BROWN,  FUR,    GRASSLAND,  MEDIUM, HERBIVORE, NO),
+    ('Urso',         MAMMAL, BROWN,  FUR,    IGNORE,     LARGE,  CARNIVORE, YES),
+    ('Tigre',        MAMMAL, ORANGE, FUR,    GRASSLAND,  MEDIUM, CARNIVORE, YES),
+    ('Rato',         MAMMAL, GREY,   FUR,    URBAN,      SMALL,  CARNIVORE, YES),
+    ('Foca',         MAMMAL, GREY,   FUR,    POLAR,      MEDIUM, CARNIVORE, IGNORE),
+    ('Leão Marinho', MAMMAL, GREY,   DRY,    POLAR,      MEDIUM, CARNIVORE, IGNORE),
+    ('Hipopótamo',   MAMMAL, BROWN,  WET,    IGNORE,     LARGE,  HERBIVORE, YES),
+    ('Rinoceronte',  MAMMAL, GREY,   DRY,    GRASSLAND,  LARGE,  HERBIVORE, YES),
+    ('Tatu',         MAMMAL, IGNORE, SHELL,  IGNORE,     TINY,   HERBIVORE, NO),
+    ('Anta',         MAMMAL, GREY,   FUR,    FOREST,     MEDIUM, HERBIVORE, NO),
+    ('Humano',       MAMMAL, IGNORE, IGNORE, FOREST,     MEDIUM, OMNIVORE,  YES),
+    ('Porco',        MAMMAL, PINK,   FUR,    URBAN,      MEDIUM, OMNIVORE,  NO),
+    ('Raposa',       MAMMAL, ORANGE, FUR,    IGNORE,     SMALL,  CARNIVORE, IGNORE),
+    ('Lobo',         MAMMAL, GREY,   FUR,    FOREST,     MEDIUM, CARNIVORE, YES),
+    ('Coelho',       MAMMAL, GREY,   FUR,    IGNORE,     MEDIUM, HERBIVORE, NO),
+    ('Morcego',      MAMMAL, BLACK,  FUR,    IGNORE,     SMALL,  OMNIVORE,  NO),
+    ('Onitorrinco',  MAMMAL, BROWN,  FUR,    IGNORE,     SMALL,  CARNIVORE, IGNORE),
+    ('Ovelha',       MAMMAL, WHITE,  FUR,    GRASSLAND,  MEDIUM, HERBIVORE, NO),
 )
-
 
 def load_animals(animals=_animals):
     for animal in animals:
